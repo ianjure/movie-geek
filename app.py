8dcacf0f994cb58eb067075ecce6c024fdff4789
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-09-08 07:59:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -60,16 +60,25 @@ center = """
         """
 st.markdown(center, unsafe_allow_html=True)
 
-page_bg_img = '''
-<style>
-body {
-background-image: url("https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_1280.jpg");
-background-size: cover;
-}
-</style>
-'''
+# ADD BACKGROUND IMAGE
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
 
-st.markdown(page_bg_img, unsafe_allow_html=True)
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+set_background('logo.svg')
 
 # [LANGCHAIN] GOOGLE API KEY CONFIGURATION
 GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
