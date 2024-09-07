5999986fd6d141dd2674ef461d88e10d4570ae2d
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-09-08 00:07:48 +0800

    test

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ movie_genres = [
 st.image(image="MovieGeek.svg", width=400)
 
 # [STREAMLIT] SUBTITLE
-st.markdown("<h5 style='text-align: center;'>Generate movie synopsis using AI.</h1>", unsafe_allow_html=True)
+st.markdown("<h5 style='text-align: center;'>Generate movie ideas using AI.</h1>", unsafe_allow_html=True)
 
 generated = False
 
