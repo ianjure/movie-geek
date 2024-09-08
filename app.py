e4418546e7301ac3364806b4333af5babc194fc5
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-09-08 08:17:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -133,12 +133,14 @@ with st.container(border=True):
         except Exception as e:
             st.error(f"An error occurred: {e}")
 
-progress_text = "Operation in progress. Please wait."
+# [STREAMLIT] SHOW PROGRESS BAR
+progress_text = "Writing the script. Please wait."
 my_bar = st.progress(0, text=progress_text)
 
 for percent_complete in range(100):
     time.sleep(0.01)
     my_bar.progress(percent_complete + 1, text=progress_text)
+    
 time.sleep(1)
 my_bar.empty()
 
