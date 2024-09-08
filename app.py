38b49429690443f6355b37fc97fc9524c598e9cf
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-09-08 08:20:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -133,19 +133,16 @@ with st.container(border=True):
         except Exception as e:
             st.error(f"An error occurred: {e}")
 
-# [STREAMLIT] SHOW PROGRESS BAR
-progress_text = "Writing the script. Please wait."
-my_bar = st.progress(0, text=progress_text)
-
-for percent_complete in range(100):
-    time.sleep(0.01)
-    my_bar.progress(percent_complete + 1, text=progress_text)
-    
-time.sleep(1)
-my_bar.empty()
-
 # [STREAMLIT] SHOW RESPONSE
 if generated:
+    progress_text = "Writing the script. Please wait."
+    my_bar = st.progress(0, text=progress_text)
+    for percent_complete in range(100):
+        time.sleep(0.01)
+        my_bar.progress(percent_complete + 1, text=progress_text)
+    time.sleep(1)
+    my_bar.empty()
+    
     content = content.replace("Synopsis:", ":").replace("*","").replace("Movie Title:","").replace("Title:","").replace('"', '')
     content_list = content.split(":")
     title = f"###{content_list[0]}"
