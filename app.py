5bc6f515a5b2f39625316b11e105930192811a20
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-09-08 20:09:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ with st.container(border=True):
             prompt = PromptTemplate.from_template(template)
     
             llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro",
-                                         temperature=1,
+                                         temperature=0.5,
                                          google_api_key=GOOGLE_API_KEY,
                                          safety_settings={HarmCategory.HARM_CATEGORY_UNSPECIFIED: HarmBlockThreshold.BLOCK_NONE,
                                                           HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE})
