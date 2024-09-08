13486dafdc89d37136d534794669168b15a95c4a
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-09-08 23:07:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -183,13 +183,13 @@ if generated:
     st.write(stream_data(title))
     st.write(stream_data(synopsis))
     
-st.divider()
-placeholder = st.empty()
-with placeholder:
-    st.markdown("<p style='text-align: center; font-size: 1rem;'>Rate the idea!</p>", unsafe_allow_html=True)
-selected = st.feedback("stars")
-sentiment_mapping = ["one", "two", "three", "four", "five"]
-if selected is not None:
-    placeholder.markdown(f"<p style='text-align: center; font-size: 1rem;'>You rated {sentiment_mapping[selected]}</p>", unsafe_allow_html=True)
+    st.divider()
+    placeholder = st.empty()
+    with placeholder:
+        st.markdown("<p style='text-align: center; font-size: 1rem;'>Rate the idea!</p>", unsafe_allow_html=True)
+    selected = st.feedback("stars")
+    sentiment_mapping = ["one", "two", "three", "four", "five"]
+    if selected is not None:
+        placeholder.markdown(f"<p style='text-align: center; font-size: 1rem;'>You rated {sentiment_mapping[selected]} for stars.</p>", unsafe_allow_html=True)
     
-    #generated = False
+    generated = True
