8813463001463d72761877f2b6cf813cafe945d9
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-09-08 20:19:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ with st.container(border=True):
             else:
                 options = ', '.join(options)
                 template = """
-                Generate a family-friendly, non-sexual, and non-explicit movie title and synopsis based on these genres:
+                Generate a family-friendly, safe for kids, and non-explicit movie title and synopsis based on these genres:
                 {genres}
                 """
             prompt = PromptTemplate.from_template(template)
