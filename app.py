041d50b5bcfa20fb61b22b0fdb1e93f4be4df235
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-09-08 20:33:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -156,16 +156,6 @@ with st.container(border=True):
 
 # [STREAMLIT] SHOW RESPONSE
 if generated:
-    """
-    progress_text = "Writing the storyline. Please wait."
-    my_bar = st.progress(0, text=progress_text)
-    for percent_complete in range(100):
-        time.sleep(0.01)
-        my_bar.progress(percent_complete + 1, text=progress_text)
-    time.sleep(1)
-    my_bar.empty()
-    """
-    
     content = content.replace("Synopsis:", "^").replace("*","").replace("Movie Title:","").replace("Title:","").replace('"', '').replace("#","").replace("\n","")
     content_list = content.split("^")
     title = f"###{content_list[0]}"
